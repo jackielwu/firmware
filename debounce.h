84bb2cb9a649d4b2779f6c7bb7addc9f19f0f3ce
jackielwu <jackielwu@gmail.com>
2019-06-22 21:32:02 -0700

    v2

--- debounce.h
+++ debounce.h
@@ -1,4 +1,13 @@
-
+#include <bool.h>
 
 enum debounce_state {stable_h, to_l, stable_l, to_h};
-const size_t threhold = 10;
+const size_t threshold = 10;
+
+/*
+ * @brief: get instant state of given gpio pin
+ *
+ * @param: pin, the gpio pin to read
+ *
+ * @return: pin state
+ */
+bool readPin(gpio_t pin);
